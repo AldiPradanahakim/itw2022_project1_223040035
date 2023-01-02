b11795e235a3496e369197e9a11e64405354f694
AldiPradanahakim <aldi.pradan.hakim26@gmail.com>
2023-01-03 04:59:29 +0700

    upload tubes project 1 portfolio

--- js/script.js
+++ js/script.js
@@ -1,33 +1,12 @@
-let menu = document.querySelector("#menu-icon");
-let navlist = document.querySelector("#header");
-
-menu.onclick = () => {
-  menu.classList.toggle("bx-x");
-  navlist.classList.toggle("open");
-};
-
-let i = 0;
-const text = document.getElementById("text");
-const typing = "From Pasundan University Students";
-
-function type() {
-  if (i < typing.length) {
-    text.innerHTML += typing.charAt(i);
-    i++;
-    setTimeout(type, 250);
-  } else {
-    setTimeout(erase, 5000);
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+  asideSectionTogglerBtn();
+});
+function asideSectionTogglerBtn() {
+  aside.classList.toggle("open");
+  navTogglerBtn.classList.toggle("open");
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.toggle("open");
   }
 }
-
-function erase() {
-  if (i > 0) {
-    text.innerHTML = text.innerHTML.slice(0, -1);
-    i--;
-    setTimeout(erase, 250);
-  } else {
-    setTimeout(type, 5000);
-  }
-}
-
-setInterval(type, 5000);
